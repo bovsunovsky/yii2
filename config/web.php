876454dab5511b14b403e2bbaf91164b8c12b199
bovsunovsky <bovsunovsky@rambler.ru>
2018-08-21 16:14:19 +0300

    Gii

--- config/web.php
+++ config/web.php
@@ -50,7 +50,7 @@ $config = [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'suffix' =>'.html',
+ //           'suffix' =>'.html',
             'rules' => [
                     [
                      'pattern' => '',
